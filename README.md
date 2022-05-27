be7525c7c4b773ea7b2d1be2855298613f741f89
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2022-05-27 12:13:34 +0800

    feat: init repository

---
+++ README.md
@@ -0,0 +1,11 @@
+# Nix Wrapper for `ansi` Script
+
+This is a Nix wrapper for [ansi](https://github.com/fidian/ansi)
+script that:
+
+> [...] will generate the proper ANSI escape sequences to move the
+> cursor around the screen, make text bold, add colors and do much
+> more. It is designed to help you colorize words and bits of text.
+
+It may be useful for users to pull it in their Nix shells and
+fancy-print more conveniently.
---
+++ default.nix
@@ -0,0 +1,35 @@
+{ ... }:
+
+let
+  ## Pinned nixpkgs:
+  pkgs = import (fetchTarball https://github.com/NixOS/nixpkgs/archive/21.11.tar.gz) { };
+
+  ## Get reference to ansi:
+  ansi = fetchTarball https://github.com/fidian/ansi/archive/3.0.1.tar.gz;
+
+  ## Package name:
+  name = "nix-print-ansi";
+
+  ## Package version:
+  version = "3.0.1";
+in
+pkgs.stdenv.mkDerivation {
+  pname = name;
+  version = version;
+
+  src = ./.;
+
+  buildInputs = [
+    pkgs.bash
+  ];
+
+  nativeBuildInputs = [
+    pkgs.makeWrapper
+  ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp ${ansi}/ansi $out/bin/ansi
+    wrapProgram $out/bin/ansi
+  '';
+}
